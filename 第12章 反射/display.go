fec91bee77c7ca1d8ed01694e18da3fd5e676c47
gongluck <1039994845@qq.com>
2020-01-16 15:50:56 +0800

    第12章 反射

---
+++ 第12章 反射/display.go
@@ -0,0 +1,185 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"reflect"
+	"strconv"
+	"sync"
+)
+
+func Display(name string, x interface{}) {
+	fmt.Printf("Display %s (%T):\n", name, x)
+	display(name, reflect.ValueOf(x))
+}
+
+func display(path string, v reflect.Value) {
+	switch v.Kind() {
+	case reflect.Invalid:
+		fmt.Printf("%s = invalid\n", path)
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < v.Len(); i++ {
+			display(fmt.Sprintf("%s[%d]", path, i), v.Index(i))
+		}
+	case reflect.Struct:
+		for i := 0; i < v.NumField(); i++ {
+			fieldPath := fmt.Sprintf("%s.%s", path, v.Type().Field(i).Name)
+			display(fieldPath, v.Field(i))
+		}
+	case reflect.Map:
+		for _, key := range v.MapKeys() {
+			display(fmt.Sprintf("%s[%s]", path,
+				formatAtom(key)), v.MapIndex(key))
+		}
+	case reflect.Ptr:
+		if v.IsNil() {
+			fmt.Printf("%s = nil\n", path)
+		} else {
+			display(fmt.Sprintf("(*%s)", path), v.Elem())
+		}
+	case reflect.Interface:
+		if v.IsNil() {
+			fmt.Printf("%s = nil\n", path)
+		} else {
+			fmt.Printf("%s.type = %s\n", path, v.Elem().Type())
+			display(path+".value", v.Elem())
+		}
+	default:
+		fmt.Printf("%s = %s\n", path, formatAtom(v))
+	}
+}
+
+func formatAtom(v reflect.Value) string {
+	switch v.Kind() {
+	case reflect.Invalid:
+		return "invalid"
+	case reflect.Int, reflect.Int8, reflect.Int16,
+		reflect.Int32, reflect.Int64:
+		return strconv.FormatInt(v.Int(), 10)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16,
+		reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return strconv.FormatUint(v.Uint(), 10)
+	case reflect.Bool:
+		if v.Bool() {
+			return "true"
+		}
+		return "false"
+	case reflect.String:
+		return strconv.Quote(v.String())
+	case reflect.Chan, reflect.Func, reflect.Ptr,
+		reflect.Slice, reflect.Map:
+		return v.Type().String() + " 0x" +
+			strconv.FormatUint(uint64(v.Pointer()), 16)
+	default:
+		return v.Type().String() + " value"
+	}
+}
+
+func main() {
+	// Some other values (YMMV)
+	Display("os.Stderr", os.Stderr)
+	// Output:
+	// Display os.Stderr (*os.File):
+	// (*(*os.Stderr).file).fd = 2
+	// (*(*os.Stderr).file).name = "/dev/stderr"
+	// (*(*os.Stderr).file).nepipe = 0
+
+	var w io.Writer = os.Stderr
+	Display("&w", &w)
+	// Output:
+	// Display &w (*io.Writer):
+	// (*&w).type = *os.File
+	// (*(*(*&w).value).file).fd = 2
+	// (*(*(*&w).value).file).name = "/dev/stderr"
+	// (*(*(*&w).value).file).nepipe = 0
+
+	var locker sync.Locker = new(sync.Mutex)
+	Display("(&locker)", &locker)
+	// Output:
+	// Display (&locker) (*sync.Locker):
+	// (*(&locker)).type = *sync.Mutex
+	// (*(*(&locker)).value).state = 0
+	// (*(*(&locker)).value).sema = 0
+
+	Display("locker", locker)
+	// Output:
+	// Display locker (*sync.Mutex):
+	// (*locker).state = 0
+	// (*locker).sema = 0
+	// (*(&locker)) = nil
+
+	locker = nil
+	Display("(&locker)", &locker)
+	// Output:
+	// Display (&locker) (*sync.Locker):
+	// (*(&locker)) = nil
+
+	ips, _ := net.LookupHost("golang.org")
+	Display("ips", ips)
+	// Output:
+	// Display ips ([]string):
+	// ips[0] = "173.194.68.141"
+	// ips[1] = "2607:f8b0:400d:c06::8d"
+
+	// Even metarecursion!  (YMMV)
+	Display("rV", reflect.ValueOf(os.Stderr))
+	// Output:
+	// Display rV (reflect.Value):
+	// (*rV.typ).size = 8
+	// (*rV.typ).ptrdata = 8
+	// (*rV.typ).hash = 871609668
+	// (*rV.typ)._ = 0
+	// ...
+
+	// a pointer that points to itself
+	type P *P
+	var p P
+	p = &p
+	if false {
+		Display("p", p)
+		// Output:
+		// Display p (display.P):
+		// ...stuck, no output...
+	}
+
+	// a map that contains itself
+	type M map[string]M
+	m := make(M)
+	m[""] = m
+	if false {
+		Display("m", m)
+		// Output:
+		// Display m (display.M):
+		// ...stuck, no output...
+	}
+
+	// a slice that contains itself
+	type S []S
+	s := make(S, 1)
+	s[0] = s
+	if false {
+		Display("s", s)
+		// Output:
+		// Display s (display.S):
+		// ...stuck, no output...
+	}
+
+	// a linked list that eats its own tail
+	type Cycle struct {
+		Value int
+		Tail  *Cycle
+	}
+	var c Cycle
+	c = Cycle{42, &c}
+	if false {
+		Display("c", c)
+		// Output:
+		// Display c (display.Cycle):
+		// c.Value = 42
+		// (*c.Tail).Value = 42
+		// (*(*c.Tail).Tail).Value = 42
+		// ...ad infinitum...
+	}
+}
